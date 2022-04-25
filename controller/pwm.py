900bbc989e830c678a79bcbc8eedff7fb4844f71
CreamyOmlette <al1g18@yandex.com>
2022-04-25 17:37:08 +0100

    gpio pwm pulses, further work: extend to 16 channels

--- controller/pwm.py
+++ controller/pwm.py
@@ -1,12 +1,33 @@
 import RPi.GPIO as GPIO
+import ctypes
 from time import sleep
+from numpy import empty
 
-def generate_pwm():
-  pin = 18				# PWM pin connected to LED
+
+def empty_loop(pulse_width):
+  for i in range(pulse_width*3):
+    x = i 
+
+def generate_pulse(addr, addr_pin, sig_pin, pulse_width, empty_loop):
+  GPIO.output(sig_pin, 0)
+  GPIO.output(addr_pin, addr)
+  GPIO.output(sig_pin, 1)
+  empty_loop(pulse_width)
+  GPIO.output(sig_pin, 0)
+  GPIO.output()
+
+def generate_waveform(pulse_width, frequency, n):
   GPIO.setwarnings(False)			#disable warnings
-  GPIO.setmode(GPIO.BTM)		#set pin numbering system
-  GPIO.setup(pin,GPIO.OUT)
-  pi_pwm = GPIO.PWM(pin, 40)		#create PWM instance with frequency
-  pi_pwm.start(0)
-  duty = 0.2				#start PWM of required Duty Cycle 
-  pi_pwm.ChangeDutyCycle(duty)
+  GPIO.setmode(GPIO.BCM)		  #set pin numbering system
+  GPIO.setup(14, GPIO.OUT)
+  GPIO.setup(18, GPIO.OUT)
+  GPIO.setup(15, GPIO.OUT)
+  GPIO.setup(17, GPIO.OUT)
+  GPIO.setup(27, GPIO.OUT)
+  rest_time = (1./frequency) - pulse_width*0.0000001 * n
+  while True:
+    generate_pulse(0, 14, 18, pulse_width, empty_loop)
+    # empty_loop(pulse_width*50)
+    # generate_pulse(1, 14, 18, pulse_width, empty_loop)
+    sleep(rest_time)
+
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from sensor.sensor_grapher import SensorGrapher
-from sensor.switcher import Switcher
-from sensor.scale import calibrate_scaling_values
-from sensor.create_motion_graphs import create_motion_graphs
-import numpy as np
-import RPi
+from controller.pwm import generate_waveform
 
 def main():
   # calibrate_scaling_values()
   # create_motion_graphs()
+  # 3 loops - 9 us, 500 - 48 us
+  generate_waveform(1, 40, 2)
   pass
 
 if(__name__ == "__main__"):
   main()
+  
