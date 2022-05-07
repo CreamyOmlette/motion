b8aa9c1d3735b34ede17fc247eb1d8df59901137
Alex Lipceanu <alexlipceanu@Alexs-MacBook-Pro-6.local>
2022-05-07 22:44:54 +0100

    controller before testing

--- controller/digit_controller.py
+++ controller/digit_controller.py
@@ -5,13 +5,10 @@ class FESController:
   pwm_max = 0
   pid: PID
   target: float
-  channel_id = -1
-  flexion = True
-  def __init__(self, pwm_max: Number, flexion: bool):
+  def __init__(self, pwm_max: Number):
     self.pwm_max = pwm_max
     self.pid = PID(0.25, 0.00001348, 0.000009)
     self.pid.output_limits = (-self.pwm_max, 0)
-    self.flexion = flexion
 
   def set_target(self, target):
     self.pid.setpoint = target
---
+++ controller/fes_system.py
@@ -0,0 +1,29 @@
+import time
+from controller.digit_controller import FESController
+from sensor.switcher import Switcher
+from controller.pwm_generator import Pwm_Generator
+from simple_pid import PID
+
+class FESSystem:
+  switcher: Switcher
+  pwm_generator: Pwm_Generator
+  controllers = []
+  targets = []
+
+  def __init__(self, switcher: Switcher = Switcher(), pwm_gen: Pwm_Generator = Pwm_Generator()):
+    self.switcher = switcher
+    self.pwm_generator = pwm_gen
+    self.channels = self.pwm_generator.get_channels()
+    controllers = [FESController(25), FESController(18), FESController(18), FESController(18), FESController(25)]
+  
+  def set_targets(self, targets):
+    self.targets = targets
+    prev = time.time()
+    dt = time.time() - prev
+    while(dt < 15):
+      roll, pitch , yaw = self.switcher.get_scaled()
+      for i, controller in enumerate(self.controllers):
+        pwm = controller(pitch[i])
+        self.pwm_generator.set_pwm(i, pwm)
+    self.pwm_generator.terminate()
+    
